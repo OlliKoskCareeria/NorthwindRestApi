d9afe8c5ece6c222b1700eb39494a12f781afe21
Olli Koski <olli.koski@student.careeria.fi>
2024-03-10 19:26:10 +0200

    Tilanne ennen jatkokehitystä

--- NorthwindRestApi/Controllers/CustomersController.cs
+++ NorthwindRestApi/Controllers/CustomersController.cs
@@ -8,7 +8,13 @@ namespace NorthwindRestApi.Controllers
     [ApiController]
     public class CustomersController : ControllerBase
     {
-        northwindContext db = new northwindContext(); 
+        //northwindContext db = new northwindContext();
+        private northwindContext db;
+
+        public CustomersController(northwindContext dbparametri)
+        {
+            db = dbparametri;
+        }
 
         [HttpGet]
         public ActionResult GetAllCustomers()
@@ -18,6 +24,67 @@ namespace NorthwindRestApi.Controllers
             return Ok(asiakkaat);
         }
 
-        
+        [HttpGet("{id}")]
+        public ActionResult GetOneCustomer(string id)
+        {
+            try
+            {
+                var asiakas = db.Customers.Find(id);
+
+                if (asiakas != null)
+                {
+                    return Ok(asiakas);
+                }
+                else
+                {
+                    return BadRequest("Asiakasta ei löydy");
+                }
+            }
+            catch(Exception ex)
+            {
+                return BadRequest("something went wrong" + ex.InnerException);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AddNew([FromBody] Customer cust)
+        {
+            try
+            {
+                db.Customers.Add(cust);
+                db.SaveChanges();
+                return Ok($"Lisättiin uusi asiakas {cust.CompanyName} from {cust.City}");
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää: " + e.InnerException);
+            }
+        }
+
+        // Poisto toiminto
+        [HttpDelete("{id}")]
+        public ActionResult Delete(string id)
+        {
+            try
+            {
+
+                var asiakas = db.Customers.Find(id);
+
+                if (asiakas != null)
+                {  // asiakas löytyy
+                    db.Customers.Remove(asiakas);
+                    db.SaveChanges();
+                    return Ok(asiakas.CompanyName + " poistettiin.");
+                }
+
+                return NotFound("Asiakasta id:llä " + id + " ei löytynyt.");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.InnerException);
+            }
+        }
+
+
     }
 }
--- NorthwindRestApi/Models/northwindContext.cs
+++ NorthwindRestApi/Models/northwindContext.cs
@@ -51,8 +51,8 @@ namespace NorthwindRestApi.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=LAPTOP-CGQK1K35\\SQLEXPRESS;Database=northwind;Trusted_Connection=True;");
+
+                //optionsBuilder.UseSqlServer("Server=LAPTOP-CGQK1K35\\SQLEXPRESS;Database=northwind;Trusted_Connection=True;");
             }
         }
 
